477b537bf3a368ca612714e2d3b370d90f76e6fd
Vitaliy <131911773+whyzzz20@users.noreply.github.com>
2023-12-12 20:21:17 +0100

    2

--- js/task-01.js
+++
@@ -1,8 +0,0 @@
-const liElem = document.querySelectorAll(".item");
-
-console.log(`Number of categories: ${liElem.length}`);
-liElem.forEach((element) => {
-  console.log("");
-  console.log(`Category: ${element.querySelector("h2").textContent}`);
-  console.log(`Elements: ${element.querySelectorAll("li").length}`);
-});
--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const controlSize = document.querySelector("#font-size-control");
-const textSize = document.querySelector("#text");
-
-controlSize.addEventListener("input", onchangeText);
-
-function onchangeText(params) {
-  const fontSize = params.target.value + "px";
-  textSize.style.fontSize = fontSize;
-}
